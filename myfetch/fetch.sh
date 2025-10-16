a6be697fd7afb4e9788e6ef6e8616daf51121aa3
Papa Senpai <TroyBranch93@gmail.com>
2025-10-16 16:25:45 -0600

    Adding my fastfetch script.

---
+++ myfetch/fetch.sh
@@ -0,0 +1,3 @@
+export ZSH="$HOME/.oh-my-zsh"
+
+pokemon-colorscripts --no-title -s -r | fastfetch -c $HOME/.config/fastfetch/config-v2.jsonc --logo-type file-raw --logo-height 10 --logo-width 5 --logo -
